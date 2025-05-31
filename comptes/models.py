0ac853db0ac6bf74586b138b4fcca82830ed4673
lou57810 <bernard.oswald@free.fr>
2025-05-31 17:02:36 +0200

    Essai intégration pgc.xlsx

--- comptes/models.py
+++ comptes/models.py
@@ -4,15 +4,15 @@ from django.db import models
 
 class Compte(models.Model):
     numero = models.CharField(max_length=10)
-    nom = models.CharField(max_length=255)
+    libelle = models.CharField(max_length=255)
 
 
-    def __str__(self):
-        return f"{self.numero} - {self.nom}"
+    # def __str__(self):
+        # return f"{self.numero} - {self.libelle}"
 
 
 class CreationCompte(models.Model):  # Comptes Clients ou Fournisseurs
     date = models.DateField()
     compte = models.ForeignKey(Compte, on_delete=models.CASCADE)  # N° compte
-    nom = models.CharField(max_length=20)
+    numero = models.CharField(max_length=20)
     libelle = models.CharField(max_length=255)
