1fc2a9eae095aeb4749d92f7fdf963e11d773b1d
lou57810 <bernard.oswald@free.fr>
2025-06-04 17:19:37 +0200

    Regul UTF8

--- comptapi/settings.py
+++ comptapi/settings.py
@@ -153,8 +153,8 @@ STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static", ]
 MEDIA_URL = '/media/'   # images téléchargées par les utilisateurs loggés
-# MEDIA_ROOT = BASE_DIR.joinpath('media/')
-MEDIA_ROOT = [BASE_DIR / "media/", ]
+MEDIA_ROOT = BASE_DIR.joinpath('media/')
+# MEDIA_ROOT = os.path.join(BASE_DIR / "media/")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- frontend/urls.py
+++ frontend/urls.py
@@ -1,5 +1,7 @@
 from . import views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -10,3 +12,6 @@ urlpatterns = [
     path('', views.accueil, name='accueil'),
     path("journaux/", include("journaux.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
